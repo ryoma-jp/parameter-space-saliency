d23c015d58001de6d6c064de2574b412f803b189
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2026-03-16 20:18:16 +0900

    Fix docker run cache paths for non-root UID/GID

--- scripts/run_resnet50.sh
+++ scripts/run_resnet50.sh
@@ -3,7 +3,11 @@
 OUTPUT_ROOT=results/resnet50
 
 rm -rf $OUTPUT_ROOT
-docker compose run --rm pss \
+docker compose run --rm -u $(id -u):$(id -g) \
+    -e HOME=/work \
+    -e XDG_CACHE_HOME=/work/.cache \
+    -e TORCH_HOME=/work/.cache/torch \
+    pss \
     python3 parameter_and_input_saliency.py \
         --model resnet50 \
         --image_path raw_images/great_white_shark_mispred_as_killer_whale.jpeg \
